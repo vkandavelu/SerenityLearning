2c5b97536019f072f9d5f5b619222442ec85174f
Venkatasubramanian <vkandavelu@hotmail.com>
2024-05-03 23:46:37 +0100

    My First Serenity Project

---
+++ src/test/java/Screenplayjunittest/SimpleScreenplaytest.java
@@ -0,0 +1,58 @@
+package Screenplayjunittest;
+
+
+        import net.serenitybdd.annotations.Managed;
+        import net.serenitybdd.annotations.Narrative;
+        import net.serenitybdd.junit.runners.SerenityRunner;
+        import net.serenitybdd.screenplay.Actor;
+        import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+        import net.serenitybdd.screenplay.actions.Click;
+        import net.serenitybdd.screenplay.actions.Enter;
+        import net.serenitybdd.screenplay.actions.Open;
+        import net.serenitybdd.screenplay.questions.Text;
+        import net.serenitybdd.screenplay.ui.Button;
+        import net.serenitybdd.screenplay.ui.PageElement;
+        import net.serenitybdd.screenplay.ui.InputField;
+        import org.junit.Before;
+        import org.junit.BeforeClass;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.firefox.GeckoDriverService;
+
+        import java.awt.*;
+
+        import static org.hamcrest.MatcherAssert.assertThat;
+        import static org.hamcrest.Matchers.equalTo;
+
+@Narrative(text={"In order to test complex interactions",
+        "As a Serenity developer",
+        "I want to interact with forms and validate responses"})
+@RunWith(SerenityRunner.class)
+public class SimpleScreenplaytest {
+
+    @Managed
+    WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        System.setProperty(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY, "D:\\AuotDriver\\geckodriver.exe");
+    }
+
+
+
+    @Test
+    public void formInteractionTest() {
+        Actor anna = Actor.named("Anna");
+        anna.can(BrowseTheWeb.with(driver));
+        anna.attemptsTo(
+                Open.url("https://www.exampleform.com"),
+                Enter.theValue("Hello World").into(InputField.withNameOrId("messageField")),
+                Click.on(Button.withNameOrId("submitButton"))
+        );
+
+        // Assuming a response is displayed in a label with id "responseLabel"
+        String response = Text.of(PageElement.withNameOrId("responseLabel")).answeredBy(anna).toString();
+        assertThat(response, equalTo("Expected Response"));
+    }
+}
---
+++ src/test/java/SimpleSerenityTest.java
@@ -0,0 +1,26 @@
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+
+@RunWith(SerenityRunner.class)
+public class SimpleSerenityTest {
+
+    @Managed
+    WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        System.setProperty(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY, "D:\\AuotDriver\\geckodriver.exe");
+    }
+
+    @Test
+    public void simpleSerenityTest() {
+        driver.get("https://www.example.com");
+        System.out.println("Title of the page: " + driver.getTitle());
+    }
+}
